443c9a552f7fff23226871eb16e7bb5eb089dfeb
Jingxuan Hu <hujingxuan1991@gmail.com>
2016-12-08 20:46:51 -0800

    lazy load

--- src/main.js
+++ src/main.js
@@ -1,16 +1,21 @@
 // Importing from Animations and Messaging
-import { MerryXmas } from './animations';
-import { Alert } from './messaging'
+//import { MerryXmas } from './animations';
+//import { Alert } from './messaging'
 
 // Listener to fire up Merry Xmas
 document.getElementById( 'button' ).addEventListener( 'click', e => {
-  if (!this.merryXmas) {
-    this.merryXmas = new MerryXmas( 'effect' );
-  }
-  this.merryXmas.animate();
+  System.import("./animations").then( Animations => {
+     if (!this.merryXmas) {
+        this.merryXmas = new Animations.MerryXmas( 'effect' );
+      }
+      this.merryXmas.animate(); 
+  });
+  
 } );
 
 // Timeout to fire up Alert
 setTimeout(() => {
-  new Alert();
+    System.import("./messaging").then( Msg => {
+        new Msg.Alert();
+    });
 }, 5000);
